83205c4e50943d717741dd5786847ed7fc814a57
Khash <khash.021@gmail.com>
2018-02-10 02:31:43 -0800

    Add comments for Bomb Class

--- src/com/company/Bomb.java
+++ src/com/company/Bomb.java
@@ -1,17 +1,29 @@
 package com.company;
 
+/**
+ *  This class is responsible for randomly selecting bomb(s) coordinates.
+ *
+ */
+
 
 public class Bomb {
 
     //default constructor
     Bomb(){}
 
-    //Generates a random number between 0 and the upper limit as an input
+    //Generates a random number between 0 and i (input)
     public static int random (int i) {
         int random = (int) (Math.random() * (i+1));
         return random;
     }
 
+    /**
+     * Function to check whether an index is in bound of the array
+     *
+     * @param size :size of the array.
+     * @param index :index to checked
+     * @return true is the given index is within bounds of the given array size
+     */
     private boolean inBounds(int size, int index){
         if (index >= 0 && index < size) {
             return true;
@@ -20,6 +32,13 @@ public class Bomb {
         }
     }
 
+    /**
+     *  Method for creating a single bomb
+     * @param size :is the size of the 2-D symmetrical array (number of rows, or columns; not the total number of
+     *             elements present in the array
+     * @return output[] :an integer array of the bomb coordinate with the format [i][j]
+     *                   (index 0 being row, index 1 being column)
+     */
     public int[] bomb1(int size) {
         int[] output = new int[2];
         int b1x = random(size - 1);
@@ -30,20 +49,31 @@ public class Bomb {
     }
 
     /**
-     * 2-grid diagonal bomb method
+     * Method for creating a 2-grid diagonal bomb. (the two bomb locations are next to each other)
+     * @param size :is the size of the 2-D symmetrical array (number of rows, or columns; not the total number of
+     *             elements present in the array
+     * @return output[] :an integer array of the bomb coordinate with the format [bomb1-i][bomb1-j][bomb2-i][bomb2-j]
      *
+     *         After generating the first bomb location, it then tries to create a second bomb using randomly
+     *         generated number (checking inBound before assigning them)
      */
     public int[] bomb2d (int size) {
+        //declaring and initializing variables
         int[] output = new int[4];
         int b2x, b2y;
+
+        //creating the 1st bomb
         int b1x = random(size - 1);
         output[0] = b1x;
         int b1y = random(size - 1);
         output[1] = b1y;
+
+        //random numbers (either 1 or 0) used for generating the 2nd bomb
         int random1 = random(1);
         int random2 = random(1);
         //2nd bomb-x
         switch (random1) {
+            //For this case it tries to subtract first, if not in bound then adds
             case 0:
                 if (this.inBounds(size, b1x - 1)) {
                     b2x = b1x - 1;
@@ -61,7 +91,7 @@ public class Bomb {
                 output[2] = b2x;
                 break;
         } //switch
-        //2nd bomb-y
+        //Same logic for 2nd bomb-y
         switch (random2) {
             case 0:
                 if (this.inBounds(size, b1y - 1)) {
@@ -83,19 +113,32 @@ public class Bomb {
         return output;
     } //bomb2d()
 
+    /**
+     * Method for creating a 2-grid linear bomb; horizontal, or vertical (the two bomb locations are next to each other)
+     * @param size :is the size of the 2-D symmetrical array (number of rows, or columns; not the total number of
+     *             elements present in the array
+     * @return output[] :an integer array of the bomb coordinate with the format [bomb1-i][bomb1-j][bomb2-i][bomb2-j]
+     *
+     *         After generating the first bomb location, it then tries to create a second bomb using randomly
+     *         generated number (checking inBound before assigning them)
+     */
     public int[] bomb2 (int size) {
+        //declaring and initializing variables
         int[] output = new int[4];
         int b2x, b2y;
+
+        //creating the 1st bomb
         int b1x = random(size - 1);
         output[0] = b1x;
         int b1y = random(size - 1);
         output[1] = b1y;
-        //rendom variables for x-y construction logic
+
+        //random numbers (either 1 or 0) used for generating the 2nd bomb
         int random1 = random(1);
         int random2 = random(1);
-        //random variable for direction logic
-        int randomHV = random(1);
 
+        //random number (1 or 0) for direction logic
+        int randomHV = random(1);
         //construction logic (0 creates vertical; 1 creates horizontal)
         switch (randomHV) {
             //vertical (b2x stays the same, calculate b2y)
@@ -148,7 +191,6 @@ public class Bomb {
         } //switch
         return output;
     } //bomb2
-
 
 
 } //main
